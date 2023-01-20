7220c6c40c8993aed0b20bc686b7557f832c7179
i.krohalev <cramush@gmail.com>
2023-01-20 13:43:34 +0300

    add logging, mongo motor to parser.py

--- parser.py
+++ parser.py
@@ -5,19 +5,31 @@ import aiofiles
 from bs4 import BeautifulSoup as bs
 import os
 import json
-from motor import motor_asyncio
+import motor.motor_asyncio
+from dotenv import load_dotenv
+import logging
+
+load_dotenv()
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+level = logging.getLevelName(os.getenv('LOGGING_LEVEL'))
+logger.setLevel(level)
 
 URL = "https://eda.ru"
 HEADERS = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"}
 
-client = motor_asyncio.AsyncIOMotorClient("mongodb://mongo:mongo@mongo_dev:27017/?authMechanism=DEFAULT")
-db = client.recipes_db
-collection = db.recipes
+
+def init_mongo_client() -> motor.motor_asyncio.AsyncIOMotorClient:
+    username = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+    password = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
+    host = os.getenv('MONGO_HOST')
+    return motor.motor_asyncio.AsyncIOMotorClient(f"mongodb://{username}:{password}@{host}:27017/?authMechanism=DEFAULT")
 
 
-async def save_recipes_info(recipes_info: list):
+async def save_recipes_info(collection: motor.motor_asyncio.core.AgnosticCollection, recipes_info: list) -> None:
     await collection.insert_many(recipes_info)
 
 
@@ -28,7 +40,7 @@ async def save_image(recipe_name: str, image_url: str, headers: dict) -> str:
                 async with aiofiles.open(f"recipes_data/{recipe_name}/" + "".join(image_url.split("/")[-3:]), 'wb') as image:
                     await image.write(await response.read())
     except Exception as ex:
-        print(f"save image error: {ex}")
+        logger.error(f"save image error: {ex}")
 
     return f"recipes_data/{recipe_name}/" + "".join(image_url.split("/")[-3:])
 
@@ -39,19 +51,19 @@ async def save_recipe_page_info(recipe_page_info: dict) -> None:
         async with aiofiles.open(f"recipes_data/{recipe_page_info['recipe_name']}/{recipe_page_info['recipe_name']}.json", 'w') as file:
             await file.write(json_obj_recipe_page_info)
     except Exception as ex:
-        print(f"save recipe page info error: {ex}")
+        logger.error(f"save recipe page info error: {ex}")
 
 
 async def get_recipes_info(url: str, recipes_urls: list, headers: dict) -> list:
     recipes_info = []
-    for recipes_url in recipes_urls:
+    for recipe_url in recipes_urls:
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.get(url=url + recipes_url, headers=headers) as response:
+                async with session.get(url=url + recipe_url, headers=headers) as response:
                     soup = bs(await response.text(), "lxml")
 
         except RuntimeError as ex:
-            print(f"get recipe page info error: {ex}")
+            logger.debug(f"get recipe page info error: {ex}")
 
         recipe_name = soup.find("h1", class_="emotion-gl52ge").text.strip().replace('\xa0', ' ')
 
@@ -61,9 +73,8 @@ async def get_recipes_info(url: str, recipes_urls: list, headers: dict) -> list:
             "cooking_time": soup.find("div", class_="emotion-my9yfq").text.strip()
         }
 
-        # recipe_id = soup.find("link", {'rel': 'canonical'}).get("href").split("-")[-1]
-        #
-        # recipe_page_info["_id"] = recipe_id
+        recipe_id = soup.find("link", {'rel': 'canonical'}).get("href").split("-")[-1]
+        recipe_page_info["_id"] = recipe_id
 
         if not os.path.exists(f"recipes_data/{recipe_name}/"):
             os.mkdir(f"recipes_data/{recipe_name}/")
@@ -79,14 +90,14 @@ async def get_recipes_info(url: str, recipes_urls: list, headers: dict) -> list:
             preview_image_url = soup.find("span", {'itemprop': 'resultPhoto'}).get("content")
             preview_image_path = await save_image(recipe_name, preview_image_url, headers)
         except AttributeError:
-            print(f'recipe "{recipe_name}" does not have preview image')
+            logger.debug(f'recipe "{recipe_name}" does not have preview image')
 
         recipe_page_info["preview_image_path"] = preview_image_path
 
         try:
             recipe_page_info["description"] = soup.find("span", class_="emotion-1x1q7i2").text.strip().replace('\xa0', ' ')
         except AttributeError:
-            print(f'recipe "{recipe_name}" does not have description')
+            logger.debug(f'recipe "{recipe_name}" does not have description')
 
         ingredients = {}
         ingredients_list = soup.findAll("span", {'itemprop': 'recipeIngredient'})
@@ -127,7 +138,7 @@ async def get_recipes_info(url: str, recipes_urls: list, headers: dict) -> list:
                     "picture", recursive=False)[0].findChildren("img", recursive=False)[0].get("src")
                 step_image_path = await save_image(recipe_name, step_image_url, headers)
             except AttributeError:
-                print(f'{step_number} in recipe "{recipe_name}" does not have image')
+                logger.debug(f'{step_number} in recipe "{recipe_name}" does not have image')
 
             step = {
                 step_number: {
@@ -143,11 +154,13 @@ async def get_recipes_info(url: str, recipes_urls: list, headers: dict) -> list:
             recipe_page_info["recipe_hint"] = soup.find("div", class_="emotion-1mtrnmn").text.strip()
         except AttributeError:
             recipe_page_info["recipe_hint"] = "no_recipe_hint"
-            print(f'recipe "{recipe_name}" does not have recipe hint')
+            logger.debug(f'recipe "{recipe_name}" does not have recipe hint')
 
         await save_recipe_page_info(recipe_page_info)
 
         recipes_info.append(recipe_page_info)
+
+        time.sleep(1)
 
     return recipes_info
 
@@ -163,12 +176,18 @@ async def get_recipes_urls(url: str, headers: dict, page: int) -> list:
                 for recipe_url in recipes_urls:
                     recipes_urls_list.append(recipe_url.get("href"))
     except Exception as ex:
-        print(f"get recipes urls error: {ex}")
+        logger.error(f"get recipes urls error: {ex}")
         return []
     return recipes_urls_list
 
 
 async def main():
+    logger.info("start parser")
+
+    client = init_mongo_client()
+    db = client.recipes_db
+    collection = db.recipes
+
     page = 714
     while True:
         recipes_urls = await get_recipes_urls(URL, HEADERS, page)
@@ -178,10 +197,11 @@ async def main():
 
         recipes_info = await get_recipes_info(URL, recipes_urls, HEADERS)
 
-        mongo_loop = client.get_io_loop()
-        mongo_loop.run_until_complete(await save_recipes_info(recipes_info))
+        await save_recipes_info(collection, recipes_info)
 
         time.sleep(1)
+
+    logger.info("stop parser")
 
 
 if __name__ == '__main__':
